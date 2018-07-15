d0020e99593e52c3f68139d12dbda2cffb9176be
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2018-07-15 14:02:17 +0200

    Aggiunti i connettori

---
+++ Riot Fetcher/src/main/java/RiotFetcher/BigDataTorloneFinal/CustomProducer.java
@@ -0,0 +1,7 @@
+package RiotFetcher.BigDataTorloneFinal;
+import org.apache.kafka.clients.producer.Producer;
+public class CustomProducer {
+	private Producer<Integer, String> producer = null;
+	private String topic;
+	
+}
