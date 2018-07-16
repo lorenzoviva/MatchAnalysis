986682b1c9a7711066ee47078b8b22a0826d9243
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2018-07-16 11:21:40 +0200

    Deleted riot fetcher

--- Riot Fetcher/src/main/java/RiotFetcher/BigDataTorloneFinal/CustomProducer.java
+++
@@ -1,7 +0,0 @@
-package RiotFetcher.BigDataTorloneFinal;
-import org.apache.kafka.clients.producer.Producer;
-public class CustomProducer {
-	private Producer<Integer, String> producer = null;
-	private String topic;
-	
-}
