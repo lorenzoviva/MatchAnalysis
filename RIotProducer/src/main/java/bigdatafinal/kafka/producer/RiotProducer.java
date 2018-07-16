dfc70227f5d42feb606b7efb7bad10b5363533f7
lorenzoviva <lorenzo.vaccaro@hotmail.com>
2018-07-16 12:17:29 +0200

    Aggiunto Riot producer

---
+++ RIotProducer/src/main/java/bigdatafinal/kafka/producer/RiotProducer.java
@@ -0,0 +1,9 @@
+package bigdatafinal.kafka.producer;
+
+public class RiotProducer extends CustomProducer {
+	private static final String TOPIC = "";
+	
+	public RiotProducer(String topic) {
+		super(TOPIC);
+	}
+}
