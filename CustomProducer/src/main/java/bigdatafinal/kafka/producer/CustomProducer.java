e016d2cad28ddc38dfae69d7419233aaaab7aa24
Edor <em.dortenzi@gmail.com>
2018-07-16 11:47:03 +0200

    CustomProducer

---
+++ CustomProducer/src/main/java/bigdatafinal/kafka/producer/CustomProducer.java
@@ -0,0 +1,34 @@
+package bigdatafinal.kafka.producer;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+public class CustomProducer {
+	
+	private Properties props;
+	private Producer<String, String> producer;
+	
+	public CustomProducer() {
+		this.props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "all");
+		props.put("retries", 0);
+		props.put("batch.size", 16384);
+		props.put("linger.ms", 1);
+		props.put("buffer.memory", 33554432);
+		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		this.producer = new KafkaProducer<String, String>(props);
+	}
+
+	protected void send(String topic, String input) {
+		producer.send(new ProducerRecord<String, String>(topic, Integer.toString(input.hashCode()), input));
+	}
+	
+	protected void close() {
+		this.producer.close();
+	}
+}
