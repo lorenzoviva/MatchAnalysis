22e3b6b361659913cce552c543d3e93f6df5c328
Edor <em.dortenzi@gmail.com>
2018-07-17 16:02:35 +0200

    Inizio manager

---
+++ Manager/src/main/java/bigdatafinal/manager/Scheduler.java
@@ -0,0 +1,15 @@
+package bigdatafinal.manager;
+
+public class Scheduler {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private String populateRiotFromTwitchId(String twitchId) {
+		
+		return null;
+	}
+
+}
