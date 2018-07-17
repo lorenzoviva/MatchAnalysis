60eb1beabd61a714dfb25b8f0120b0badc6636ae
Edor <em.dortenzi@gmail.com>
2018-07-17 18:42:55 +0200

    .

---
+++ CustomConsumer/src/main/java/bigdatafinal/kafka/consumer/TwitchIDListener.java
@@ -0,0 +1,22 @@
+package bigdatafinal.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.utils.Scheduler;
+import org.json.JSONObject;
+
+public class TwitchIDListener extends CustomConsumer {
+
+	public TwitchIDListener(String[] topics, String group) {
+		super(topics, group);
+	}
+
+	@Override
+	public void processMessage(ConsumerRecord<String, String> record) {
+		Scheduler scheduler = Scheduler.getInstance();
+		JSONObject jsonObject = new JSONObject(record.value());
+		jsonObject.getString("user_id");
+		
+	}
+	
+
+}
